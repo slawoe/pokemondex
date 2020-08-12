2c26ee19e5d4c5b109754cc4c3588b329d56144d
slawoe <info@slawomirernst.de>
2020-08-12 13:36:29 +0200

    Add List component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./App.css";
+import List from "./components/List";
 
 function App() {
-  return <div>Pokedex</div>;
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Pokedex</h1>
+        <input className="input" placeholder="Search your Pokemon..." />
+      </header>
+      <main className="pokeList">
+        <List>Lecko mio</List>
+        {/* <List>
+          <ListItem href="#">
+            <ListImg src="">
+              <ListItemText primary="Bulbasaur" secondary="JavaScript"></ListItemText>
+          </ListItem>
+        </List> */}
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function List(props) {
+  return <nav>{props.children}</nav>;
+}
+
+export default List;
+
+/* <nav>
+<a href="#">
+  <img></img>
+  <span></span>
+</a>
+<a href="#">
+  <img></img>
+  <span></span>
+</a>
+<a href="#">
+  <img></img>
+  <span></span>
+</a>
+</nav> */
