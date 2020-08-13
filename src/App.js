869141819e457ba3617eacbd382135f535ec95e1
slawoe <info@slawomirernst.de>
2020-08-13 11:00:28 +0200

    Map pokemons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,53 @@ import ListItemText from "./components/ListItemText";
 import ListIcon from "./components/ListIcon";
 import Bulbasaur from "../src/assets/bulbasaur.png";
 
+const bulbasaur = {
+  name: "Bulbasaur",
+  imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+  id: "001",
+  link: "#bulbasaur",
+};
+
+const ivysaur = {
+  name: "Ivysaur",
+  imgSrc: "https://img.pokemondb.net/artwork/large/ivysaur.jpg",
+  id: "002",
+  link: "#ivysaur",
+};
+
+const pokemons = [ivysaur, bulbasaur];
+// const pokemons = [
+//   {
+//     name: "Bulbasaur",
+//     imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
+//     id: "001",
+//     link: "#bulbasaur",
+//   },
+//   {
+//     name: "Ivysaur",
+//     imgSrc: "https://img.pokemondb.net/artwork/large/ivysaur.jpg",
+//     id: "002",
+//     link: "#ivysaur",
+//   },
+// ];
+
+const pokemonListFromArray = pokemons.map((pokemon) => console.log(pokemon));
+
 function App() {
+  // const listItems = pokemons.map((pokemon) => (
+  //   <ListItem href={pokemon.link}>
+  //     <ListImg
+  //       src={pokemon.imgSrc}
+  //       alt={`Picture of ${pokemon.name}`}
+  //     ></ListImg>
+  //     <ListItemText
+  //       primary={pokemon.name}
+  //       secondary={`#${pokemon.id}`}
+  //     ></ListItemText>
+  //     <ListIcon src="" alt="#"></ListIcon>
+  //   </ListItem>
+  // ));
+
   return (
     <div className="app">
       <header className="header">
@@ -16,20 +62,19 @@ function App() {
       </header>
       <main className="pokeList">
         <List>
-          <ListItem href="#">
-            <ListImg src={Bulbasaur} alt="#"></ListImg>
-            <ListItemText
-              primary="Whiskey"
-              secondary="Schluckspecht"
-            ></ListItemText>
-            <ListIcon src="" alt="#"></ListIcon>
-          </ListItem>
-
-          <ListItem href="#">
-            <ListImg src="" alt="#"></ListImg>
-            <ListItemText primary="Döner" secondary="Futtertier"></ListItemText>
-            <ListIcon src="" alt="#"></ListIcon>
-          </ListItem>
+          {pokemons.map((pokemon) => (
+            <ListItem key={pokemon.id} href={pokemon.link}>
+              <ListImg
+                src={pokemon.imgSrc}
+                alt={`Picture of ${pokemon.name}`}
+              ></ListImg>
+              <ListItemText
+                primary={pokemon.name}
+                secondary={`#${pokemon.id}`}
+              ></ListItemText>
+              <ListIcon src="" alt="#"></ListIcon>
+            </ListItem>
+          ))}
         </List>
       </main>
     </div>
