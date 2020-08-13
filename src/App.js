c34faab6767bad1df6b1f0000a374f93ec58f4c0
slawoe <info@slawomirernst.de>
2020-08-13 16:48:58 +0200

    Add loader

--- src/App.js
+++ src/App.js
@@ -5,28 +5,15 @@ import ListItem from "./components/ListItem";
 import ListImg from "./components/ListImg";
 import ListItemText from "./components/ListItemText";
 import ListIcon from "./components/ListIcon";
-import Bulbasaur from "../src/assets/bulbasaur.png";
+import { fetchPokemons } from "../src/api/Pokemons";
 
-const bulbasaur = {
-  name: "Bulbasaur",
-  imgSrc: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
-  id: "001",
-  link: "#bulbasaur",
-};
-
-const ivysaur = {
-  name: "Ivysaur",
-  imgSrc: "https://img.pokemondb.net/artwork/large/ivysaur.jpg",
-  id: "002",
-  link: "#ivysaur",
-};
-
-const pokemons = [ivysaur, bulbasaur];
-
-const pokemonListFromArray = pokemons.map((pokemon) => console.log(pokemon));
-
+function waitFor(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
 function App() {
-  // mögliche Schreibweise, jedoch nicht genutzt
+  const [pokemons, setPokemons] = React.useState(null);
+  const [IsLoaded, setIsLoaded] = React.useState(false);
+  // mögliche Schreibweise außerhalb unseres Returns, jedoch nicht genutzt
   // const listItems = pokemons.map((pokemon) => (
   //   <ListItem href={pokemon.link}>
   //     <ListImg
@@ -41,15 +28,50 @@ function App() {
   //   </ListItem>
   // ));
 
+  // const handleClick = async () => {
+  //   const allPokemons = await fetchPokemons();
+  //   setPokemons(allPokemons);
+  // };
+
+  // Leons Weg
+  React.useEffect(() => {
+    async function fetchData() {
+      await waitFor(5000);
+      const allPokemons = await fetchPokemons();
+      setIsLoaded(true);
+      setPokemons(allPokemons);
+    }
+    fetchData();
+  }, []);
+
+  // mein Weg
+  // useEffect(() => {
+  //   const timer = setTimeout(async () => {
+  //     const allPokemons = await fetchPokemons();
+  //     setPokemons(allPokemons);
+  //     console.log(`After 5 second`);
+  //   }, 5000);
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  if (!IsLoaded) {
+    return (
+      <h1 className="title loading">
+        Aren't you too old for Pokemons? Ok... If you really need it: I'm
+        loading, kid!
+      </h1>
+    );
+  }
   return (
     <div className="app">
       <header className="header">
         <h1 className="title">Pokedex</h1>
         <input className="input" placeholder="Search your Pokemon..." />
+        {/* <button onClick={handleClick}>Load pokemons</button> */}
       </header>
       <main className="pokeList">
         <List>
-          {pokemons.map((pokemon) => (
+          {pokemons?.map((pokemon) => (
             <ListItem key={pokemon.id} href={pokemon.link}>
               <ListImg
                 src={pokemon.imgSrc}
