ab9834f9cc77f6139a12e410a8b7c39ffdadf11f
slawoe <info@slawomirernst.de>
2020-08-17 10:13:52 +0200

    Add router

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoadingScreen({ children }) {
+  return (
+    <h1 className="title loading">
+      Aren't you too old for Pokemons? Ok... If you really need it: I'm loading,
+      kid!
+    </h1>
+  );
+}
+
+export default LoadingScreen;
