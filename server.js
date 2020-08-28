14af2418797f5308765ae1c9187bd9f883b8550b
slawoe <info@slawomirernst.de>
2020-08-28 11:04:49 +0200

    Add app.listen to server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.listen(port, function () {
+  console.log("YAAAS, App is listening on http://localhost:3000");
+});
