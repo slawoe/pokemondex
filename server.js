0d1dbaf10c113678d78e5770c31397bf56cfdfcb
slawoe <info@slawomirernst.de>
2020-08-28 11:20:50 +0200

    Add body-parser, MongoClient & useUnifiedTopology to server.js

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+});
+
 const app = express();
+app.use(bodyParser.json());
 
 const port = 3000;
 
 app.listen(port, function () {
-  console.log("YAAAS, App is listening on http://localhost:3000");
+  console.log(`YAAAS, App is listening on http://localhost:${port}`);
+});
+
+app.get("/", (request, response) => {
+  console.log("Request /");
+  response.send("Benne hat mich lieb, hat er gesagt!");
 });
